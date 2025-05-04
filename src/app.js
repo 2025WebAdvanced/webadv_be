70eda866356eaf460e219d84fff23f40263a6c1d
wooki_02 <hyunook02@naver.com>
2025-05-04 10:14:25 +0000

    chore(#1): 서버 데이터베이스 세팅

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const app = express();
+const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'ho020905',
+	port: 3306,
+	database: 'test_db'
+});
+
+const app = express();
 app.set('port', 8080);
+
+const getAllUsers = () => {
+	connection.query('select * from users order by id desc', (err, rows, fields) => {
+		if(err) throw err;
+		return rows;
+	});
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, Express')
+})
+
+app.get('/getUsers', (req, res) => {
+    res.json(getAllUsers());
 })
 
 app.listen(app.get('port'), () => {
