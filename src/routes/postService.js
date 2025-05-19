4c30c42c42842910c1a7f36736bf2ce8b362b5e1
wooki <hyunook02@naver.com>
2025-05-20 04:03:44 +0900

    feat(#47): 게시글 작성 서비스 구현

--- src/routes/postService.js
+++ src/routes/postService.js
@@ -1,22 +1,37 @@
 // src/routes/post.js
 const express = require('express');
 const router = express.Router();
+const Post = require('../model/postModel');
 
 router.post('/', (req, res) => {
-  const { title, content, author } = req.body;
+  const { title, content, userId } = req.body;
 
-  if (!title || !content || !author) {
+  if (!title || !content || !userId) {
     res.status(400).json({
       code: 1900,
       message: '필수 입력값이 없습니다.',
     });
   }
 
-  // TODO: DB에 게시글 저장 로직
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    userId: req.body.userId,
+  });
 
-  res.json({
-    code: 2001,
-    message: '게시글 작성에 성공했습니다.',
+  Post.create(post, (err, data) => {
+    if (err) {
+      res.status(500).send({
+        message: err.message || "알 수 없는 에러 발생",
+      });
+    }
+    if (data) {
+      res.json({
+        code: 2001,
+        message: '게시글 작성에 성공했습니다.',
+        postId: data.id,
+      });
+    }
   });
 });
 
