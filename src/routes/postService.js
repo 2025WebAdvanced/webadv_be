6fb4b46da1f7952e5878878f9a20b85ef8b33dc2
wooki <hyunook02@naver.com>
2025-05-25 04:41:07 +0900

    feat(#23): postUpdate -> postService 및 postModel로 병합

--- src/routes/postService.js
+++ src/routes/postService.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../model/postModel');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/', (req, res) => {
-  const { title, content, userId } = req.body;
+// 게시글 작성
+router.post('/', authMiddleware, (req, res) => {
+  const { title, content } = req.body;
 
-  if (!title || !content || !userId) {
+  if (!title || !content) {
     return res.status(400).json({
       code: 1900,
       message: '필수 입력값이 없습니다.',
@@ -35,8 +37,14 @@ router.post('/', (req, res) => {
   });
 });
 
-router.get('/:postId', (req, res) => {
+
+// 게시글 조회
+router.get('/:postId', (req, res, next) => {
+  if (req.headers.authorization)
+    authMiddleware(req, res, next);
+}, (req, res) => {
   const { postId } = req.params;
+  const userId = req.user?.userId;
 
   Post.findById(postId, (err, data) => {
     if (err) {
@@ -48,7 +56,10 @@ router.get('/:postId', (req, res) => {
       res.json({
         code: 1009,
         message: '게시글을 성공적으로 조회했습니다.',
-        data: data,
+        data: {
+          ...data,
+          isAuthor: userId && userId === data.userId,
+        }
       })
     } else {
       return res.status(404).json({
@@ -56,6 +67,50 @@ router.get('/:postId', (req, res) => {
       })
     }
   })
+});
+
+// 게시글 수정
+router.post('/:postId', authMiddleware, async (req, res) => {
+  const { postId } = req.params;
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({
+      code: 1900,
+      message: '필수 입력값이 없습니다.',
+    });
+  }
+
+  Post.findById(postId, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        message: err.message || "알 수 없는 에러 발생",
+      });
+    }
+    if (data) {
+      if (rows[0].userId !== req.user.userId) {
+        return res.status(403).json({
+          code: 4003,
+          message: '본인의 게시글만 수정할 수 있습니다.',
+        });
+      } else {
+        Post.updatePost({ ...data.post, title: title, content: content,},
+          (e, d) => {
+            if (e)
+              return res.status(500).json({ message: e.message || "알 수 없는 에러 발생" });
+            return res.json({
+              code: 2002,
+              message: '게시글이 수정되었습니다.',
+            });
+        });
+      }
+    } else {
+      return res.status(404).json({
+        code: 4004,
+        message: '게시글을 찾을 수 없습니다.',
+      });
+    }
+  });
 });
 
 module.exports = router;
