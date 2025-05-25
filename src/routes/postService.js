6c6a2ac564585dae4d3ab08a88cce03869b41a79
wooki <hyunook02@naver.com>
2025-05-25 18:31:48 +0900

    feat(#24): 게시글 삭제 서비스 작성

--- src/routes/postService.js
+++ src/routes/postService.js
@@ -113,4 +113,39 @@ router.post('/:postId', authMiddleware, async (req, res) => {
   });
 });
 
+router.delete('/:postId', authMiddleware, async (req, res) => {
+  const { postId } = req.params;
+
+  Post.findById(postId, (err, post) => {
+    if (err) {
+      return res.status(404).json({
+        code: 4004,
+        message: '게시글을 찾을 수 없습니다.',
+      });
+    }
+    if (post) {
+      if (post.userId !== req.user.id) {
+        return res.status(403).json({
+          code: 4003,
+          message: '본인 게시글만 삭제할 수 있습니다.',
+        });
+      } else {
+        Post.delete(postId, (err, data) => {
+          if (err) {
+            return res.status(500).json({
+              code: 5000,
+              message: '서버 오류가 발생했습니다.',
+            })
+          } else {
+            return res.json({
+              code: 2003,
+              message: '게시글이 삭제되었습니다.',
+            })
+          }
+        })
+      }
+    }
+  });
+});
+
 module.exports = router;
