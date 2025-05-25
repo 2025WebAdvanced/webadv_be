44104db7b05d5ce96c41d028ca46aa8be1ca6227
wooki <hyunook02@naver.com>
2025-05-25 16:20:25 +0900

    feat(#28): 댓글 수정 api 병합 및 테스트

--- src/routes/commentService.js
+++ src/routes/commentService.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const db = require('../config/db');
+const Comment = require('../model/commentModel');
 
-router.post('/write', authMiddleware, async (req, res) => {
+router.post('/', authMiddleware, async (req, res) => {
   const { postId, content } = req.body;
-  const userId = req.user.userId; // JWT 토큰에서 가져온 사용자 이름
+  const userId = req.user.id;
 
   if (!postId || !content) {
     return res.status(400).json({
@@ -34,6 +34,52 @@ router.post('/write', authMiddleware, async (req, res) => {
       })
     }
   });
+});
+
+router.post('/:commentId', authMiddleware, async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({
+      code: 1900,
+      message: '필수 입력값이 없습니다.',
+    });
+  }
+
+  Comment.getCommentById(commentId, (getCommentErr, getCommentRes) => {
+    if(getCommentErr) {
+      return res.status(404).json({
+        code: 3004,
+        message: '댓글이 존재하지 않습니다.',
+      });
+    } else {
+      if (getCommentRes.userId !== req.user.id) {
+        return res.status(401).json({
+          code: 3005,
+          message: '본인 댓글만 수정할 수 있습니다.',
+        });
+      } else {
+        const comment = {
+          ...getCommentRes,
+          content: content,
+        }
+        Comment.update(comment, (err, data) => {
+          if (err) {
+            return res.status(500).json({
+              code: 5000,
+              message: err || "알 수 없는 서버 오류가 발생했습니다.",
+            });
+          } else {
+            return res.json({
+              code: 3002,
+              message: '댓글 수정에 성공했습니다.',
+            });
+          }
+        });
+      }
+    };
+  })
 });
 
 module.exports = router;
