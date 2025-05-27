5d44a119da7055e05b2ea6b9e9d3001301cbbe1f
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-05-27 14:47:58 +0900

    feat: 댓글 조회 - DB 접근

--- src/model/commentModel.js
+++ src/model/commentModel.js
@@ -1,56 +1,90 @@
 const sql = require('../config/db');
 
+// callback 기반 기존 함수들
 const Comment = function(comment) {
   this.content = comment.content;
 }
 
 Comment.create = (comment, result) => {
-  sql.query('INSERT INTO Comments (content, userId, postId) VALUES (?, ?, ?)'
-    , [comment.content, comment.userId, comment.postId]
-    , (err, res) => {
+  sql.query(
+    'INSERT INTO Comments (content, userId, postId) VALUES (?, ?, ?)',
+    [comment.content, comment.userId, comment.postId],
+    (err, res) => {
       if (err) {
         console.log('error occured in Comment.create', err);
         result(err, null);
-      }
-      else
+      } else {
         result(null, res);
+      }
     }
   );
 }
 
 Comment.update = (comment, result) => {
-  sql.query('UPDATE Comments SET content=? WHERE id=?'
-    , [comment.content, comment.id]
-    , (err, res) => {
+  sql.query(
+    'UPDATE Comments SET content=? WHERE id=?',
+    [comment.content, comment.id],
+    (err, res) => {
       if (err) {
         console.log('error occured in Comment.update', err);
         result(err, null);
-      }
-      else
+      } else {
         result(null, res);
+      }
     }
-  )
+  );
 }
 
 Comment.delete = (commentId, result) => {
-  sql.query('DELETE From Comments WHERE id=?', [commentId], (err, res) => {
-    if (err) {
-      console.log('error occured in Comment.delete', err);
-      result(err, null);
-    } else
-      result(null, res);
-  })
+  sql.query(
+    'DELETE FROM Comments WHERE id=?',
+    [commentId],
+    (err, res) => {
+      if (err) {
+        console.log('error occured in Comment.delete', err);
+        result(err, null);
+      } else {
+        result(null, res);
+      }
+    }
+  );
 }
 
 Comment.getCommentById = (commentId, result) => {
-  sql.query('SELECT * FROM Comments WHERE id=?', [commentId], (err, res) => {
-    if (err) {
-      console.log('error oucured in Comment.getCommentById', err);
-      result(err, null);
+  sql.query(
+    'SELECT * FROM Comments WHERE id=?',
+    [commentId],
+    (err, res) => {
+      if (err) {
+        console.log('error occured in Comment.getCommentById', err);
+        result(err, null);
+      } else {
+        result(null, res[0]);
+      }
     }
-    else
-      result(null, res[0]);
-  })
+  );
 }
 
-module.exports = Comment;
+// async/await 기반 함수들은 별도 함수로 정의
+const getCommentsByPostId = async (postId, limit, offset) => {
+  const [comments] = await sql.query(
+    'SELECT * FROM Comments WHERE postId = ? ORDER BY createdAt ASC LIMIT ? OFFSET ?',
+    [postId, limit, offset]
+  );
+  return comments;
+};
+
+const getTotalCommentsCount = async (postId) => {
+  const [[result]] = await sql.query(
+    'SELECT COUNT(*) AS total FROM Comments WHERE postId = ?',
+    [postId]
+  );
+  return result.total;
+};
+
+// callback 기반 함수는 Comment 객체로, async 함수들은 별도로 export
+module.exports = {
+  Comment,
+  getCommentsByPostId,
+  getTotalCommentsCount,
+};
