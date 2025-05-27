bd1bc570ae681786c46a7b65972c50f93f43bcb6
SeoMinSeok6672 <aa01055726672@gmail.com>
2025-05-27 14:47:58 +0900

    feat: 댓글 조회 - DB 접근

--- src/model/commentModel.js
+++ src/model/commentModel.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const sql = require('../config/db');
 
 const Comment = function(comment) {
@@ -16,41 +17,26 @@ Comment.create = (comment, result) => {
         result(null, res);
     }
   );
-}
+// src/model/commentModel.js
+const db = require('../config/db');
 
-Comment.update = (comment, result) => {
-  sql.query('UPDATE Comments SET content=? WHERE id=?'
-    , [comment.content, comment.id]
-    , (err, res) => {
-      if (err) {
-        console.log('error occured in Comment.update', err);
-        result(err, null);
-      }
-      else
-        result(null, res);
-    }
-  )
-}
+const getCommentsByPostId = async (postId, limit, offset) => {
+  const [comments] = await db.query(
+    'SELECT * FROM Comments WHERE postId = ? ORDER BY createdAt ASC LIMIT ? OFFSET ?',
+    [postId, limit, offset]
+  );
+  return comments;
+};
 
-Comment.delete = (commentId, result) => {
-  sql.query('DELETE From Comments WHERE id=?', [commentId], (err, res) => {
-    if (err) {
-      console.log('error occured in Comment.delete', err);
-      result(err, null);
-    } else
-      result(null, res);
-  })
-}
+const getTotalCommentsCount = async (postId) => {
+  const [[result]] = await db.query(
+    'SELECT COUNT(*) AS total FROM Comments WHERE postId = ?',
+    [postId]
+  );
+  return result.total;
+};
 
-Comment.getCommentById = (commentId, result) => {
-  sql.query('SELECT * FROM Comments WHERE id=?', [commentId], (err, res) => {
-    if (err) {
-      console.log('error oucured in Comment.getCommentById', err);
-      result(err, null);
-    }
-    else
-      result(null, res[0]);
-  })
-}
-
-module.exports = Comment;
+module.exports = {
+  getCommentsByPostId,
+  getTotalCommentsCount,
+};
