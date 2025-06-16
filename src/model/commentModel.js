28c4f6f0e0f3ec96398bb1e1fcd302c1f4bc2def
wooki <hyunook02@naver.com>
2025-06-17 01:17:07 +0900

    feat: 게시글 자세히 보기 - username 반환

--- src/model/commentModel.js
+++ src/model/commentModel.js
@@ -10,7 +10,21 @@ Comment.create = (comment, result) => {
       console.log('error occured in Comment.create', err);
       result(err, null);
     } else {
-      result(null, res);
+      db.query('UPDATE Posts SET comments = Posts.comments+1 WHERE id = ?', [comment.postId], (comerr, comres) => {
+        if (err) {
+          console.log('error occured in Comment.create', comerr);
+          result(err, null);
+        } else {
+          db.query('SELECT Comments.*, username FROM Comments JOIN Users ON userId = Users.id WHERE Comments.id=?', [res.insertId], (selErr, selRes) => {
+            if (selErr) {
+              console.log('error occured in Comment.create', selErr);
+              result(err, null);
+            } else {
+              result(null, selRes[0]);
+            }
+          })
+        }
+      })
     }
   });
 };
@@ -27,12 +41,19 @@ Comment.update = (comment, result) => {
 };
 
 Comment.delete = (commentId, result) => {
-  db.query('DELETE From Comments WHERE id=?', [commentId], (err, res) => {
+  db.query('UPDATE Posts SET comments = Posts.comments-1 WHERE id=(SELECT postId FROM Comments WHERE Comments.id=?)', [commentId], (err, res) => {
     if (err) {
       console.log('error occured in Comment.delete', err);
       result(err, null);
     } else {
-      result(null, res);
+      db.query('DELETE From Comments WHERE id=?', [commentId], (err, res) => {
+        if (err) {
+          console.log('error occured in Comment.delete', err);
+          result(err, null);
+        } else {
+          result(null, res);
+        }
+      });
     }
   });
 };
@@ -48,16 +69,31 @@ Comment.getCommentById = (commentId, result) => {
   });
 };
 
+Comment.getAllCommentsByPostId = (postId, result) => {
+  db.query(
+      `SELECT Comments.*, Users.username FROM Comments JOIN Users ON Users.id = userId WHERE postId = ? ORDER BY createdAt`,
+      [postId],
+      (err, res) => {
+        if (err) {
+          console.log('error occured in Comment.getAllCommentsByPostId', err);
+          result(err, null);
+        } else {
+          result(null, res);
+        }
+      }
+  );
+}
+
 // async/await 함수들
-const getCommentsByPostId = async (postId, limit, offset) => {
+Comment.getCommentsByPostId = async (postId, limit, offset) => {
   const [comments] = await db.query(
-    'SELECT * FROM Comments WHERE postId = ? ORDER BY createdAt ASC LIMIT ? OFFSET ?',
+    'SELECT Comments.*, Users.username FROM Comments JOIN Users ON Users.id = userId WHERE postId = ? ORDER BY createdAt LIMIT ? OFFSET ?',
     [postId, limit, offset]
   );
   return comments;
 };
 
-const getTotalCommentsCount = async (postId) => {
+Comment.getTotalCommentsCount = async (postId) => {
   const [[result]] = await db.query(
     'SELECT COUNT(*) AS total FROM Comments WHERE postId = ?',
     [postId]
@@ -65,9 +101,5 @@ const getTotalCommentsCount = async (postId) => {
   return result.total;
 };
 
-module.exports = {
-  Comment,
-  getCommentsByPostId,
-  getTotalCommentsCount,
-};
+module.exports = Comment;
 
